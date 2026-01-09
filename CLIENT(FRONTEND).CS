7b1af0c95b9735afd308c6fe97b7af5c6503d7e5
routashish2457-oss <routashish2457@gmail.com>
2026-01-09 13:21:56 +0530

    Initialize Blazor WebAssembly app with HttpClient

---
+++ CLIENT(FRONTEND).CS
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5001") // change to your API URL
+});
+
+await builder.Build().RunAsync();
